77784c473327fedfacfeba4ccc1abdd5c5c96f3a
lordmanky <graham121@hotmail.com>
2020-10-24 23:23:21 +0100

    add ns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from  '@angular/material/toolbar';
+import { MatIconModule} from  '@angular/material/icon';
+import { MatSidenavModule} from  '@angular/material/sidenav';
+import { MatListModule} from  '@angular/material/list';
+import { MatButtonModule } from  '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule, 
+    MatSidenavModule, 
+    MatListModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
