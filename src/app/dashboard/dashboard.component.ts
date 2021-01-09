5fc61ec27f275e289dd54909b9cd3ea811502675
GrahamLaw <graham121@hotmail.com>
2021-01-09 16:19:57 +0000

    latest

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,9 +19,9 @@ export class DashboardComponent implements OnInit {
   duration: string;
   quest: any;
   mode: string;
-  public userAnswer: string = "";
-  public errorAnswer: string = "";
-  correct: boolean = true;
+  public userAnswer = "";
+  public errorAnswer = "";
+  correct = true;
 
   pager = {
     index: 0,
@@ -31,26 +31,26 @@ export class DashboardComponent implements OnInit {
 
   constructor(private questService: QuestService, private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.questName = 'data/quest.json';
     this.mode = 'quest';
-    this.loadQuiz(this.questName);
+    this.loadQuiz(this.questName)
   }
 
-  loadQuiz(questName: string) {
+  loadQuiz(questName: string): void  {
     console.log(questName);
     this.questService.get(questName).subscribe(res => {
-      this.quest = new Quest(res);
+      this.quest = new Quest(res);      
       this.pager.count = this.quest.questions.length;
     
       this.startTime = new Date();
-      this.ellapsedTime = '00:00'; 
-      // this.timer = setInterval(() => { this.tick(); }, 1000);
+      this.ellapsedTime = '00:00:00'; 
+      this.timer = setInterval(() => { this.tick(); }, 1000);
      // this.duration = this.parseTime(this.config.duration);
     });
   }
 
-  get filteredQuestions() {
+  get filteredQuestions(): void  {
     console.log(this.pager.index);
     console.log(this.quest.questions.slice(this.pager.index, this.pager.index + this.pager.size));
     return (this.quest.questions) ?
@@ -58,7 +58,7 @@ export class DashboardComponent implements OnInit {
   }
 
 
-  goTo(index: number) {
+  goTo(index: number): void  {
     console.log('index');
     console.log(index);
 
@@ -72,35 +72,38 @@ export class DashboardComponent implements OnInit {
     console.log(this.pager.index);
   }
 
-/*   tick() {
+  tick() : void {
     const now = new Date();
     const diff = (now.getTime() - this.startTime.getTime()) / 1000;
-    if (diff >= this.config.duration) {
+/*     if (diff >= this.config.duration) {
       this.onSubmit();
-    }
+    } */
     this.ellapsedTime = this.parseTime(diff);
-  } */
+    console.log(this.ellapsedTime);
+  } 
 
-  parseTime(totalSeconds: number) {
+  parseTime(totalSeconds: number): string {
+    let hours: string | number = Math.floor(totalSeconds / (60*60));
     let mins: string | number = Math.floor(totalSeconds / 60);
     let secs: string | number = Math.round(totalSeconds % 60);
+    hours = (hours < 10 ? '0' : '') + hours;
     mins = (mins < 10 ? '0' : '') + mins;
-    secs = (secs < 10 ? '0' : '') + secs;
-    return `${mins}:${secs}`;
+    secs = (secs < 10 ? '0' : '') + secs;    
+    return `${hours}:${mins}:${secs}`;
   }
 
-  onStart(){
+  onStart(): void {
     this.mode = 'quest';
   }
 
-  onChange() {
+  onChange(): void  {
     console.log('changedddddd');
     console.log(this.userAnswer);
     this.correct = true;
   }
 
   
-  onAnswer() {
+  onAnswer(): void  {
     console.log(this.userAnswer);
     console.log(this.filteredQuestions[0].answer);
     if (this.userAnswer == this.filteredQuestions[0].answer)
@@ -118,8 +121,15 @@ export class DashboardComponent implements OnInit {
   }
 
   
-  onResults() {
+  onResults(): void  {
     this.router.navigate(['results']);
+  }
+
+  hintClick(): void  {
+    console.log("hint click");
+    let dialogRef = dialog.open(YourDialog, {
+      data: { name: 'austin' },
+    });
   }
 
 }
--- src/app/models/quest.ts
+++ src/app/models/quest.ts
@@ -5,12 +5,14 @@ export class Quest {
     name: string;
     description: string;
     questions: Question[];
+    hintCount: number;
 
     constructor(data: any) {
         if (data) {
             this.id = data.id;
             this.name = data.name;
             this.description = data.description;
+            this.hintCount = data.hintCount;
             this.questions = [];
             data.questions.forEach(q => {
                 this.questions.push(new Question(q));
